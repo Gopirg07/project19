edd3b3fdf4a30248d7addc80745035ee3c889de9
Gopirg07 <gopinathofficial16@gmail.com>
2022-11-26 18:10:27 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
 import { useState } from "react"; 
 import { Movielist } from './Movielist';
 
@@ -11,23 +12,40 @@ function App() {
   // ]
 
   return (
-    <div className="App"> 
-    {/* {names.map((nm)=> (
-      <Welcome name={nm.name} img={nm.img}/>
-    ))}  */}
+    <div className="App">  
+    {/* <Movielist/> */}
 
-
-    <Movielist/>
-
-    {/* <Addcolor/> */}
-
+    <ul>
+      <li>
+      <Link to={"/"}>Home</Link>
+      </li>
+      <li>
+      <Link to={"/Moviess"}>Moviess</Link>
+      </li>
+      <li>
+      <Link to={"/Add"}>Add</Link>
+      </li>
+    </ul>
+    <Routes>  
+        <Route path="/" element={<Home />} /> 
+        <Route path="/Moviess" element={<Movielist />} /> 
+        <Route path="/Films" element={<Movielist />} /> 
+        <Route path="/Add" element={<Addcolor />} /> 
+      </Routes>
     </div>
     
   );
 }
 
+function Home(){
+  return(
+    <h1>Welcome To The Movie App</h1>
+  )
+}
+
 function Addcolor(){
   const [color,setColor]=useState("red");
+  const [colorList, setColorList] = useState(["grey","black","pink","blue","yellow"])
   const styles={
     background:color,
   }
@@ -35,13 +53,25 @@ function Addcolor(){
     
     <div>
       <input 
-        style={styles}
         onChange={(event)=>setColor(event.target.value)}
+        style={styles}
         placeholder='Enter the color you want'
         value={color}
       />
+      <button onClick={()=>setColorList([...colorList,color])}>add</button>
+      {colorList.map((mn)=> <Colorbox colorr={mn}/> )}
     </div>
 
+  );
+}
+function Colorbox({colorr}){
+  const styles={
+    background:colorr,
+    height:"50px",
+    width:"250px",
+  }
+  return(
+    <div style={styles}> </div>
   );
 }
 
--- src/Movie.js
+++ src/Movie.js
@@ -3,12 +3,15 @@ import { useState } from "react";
 
 export function Movie({ movie }) {
   const [show, setShow] = useState(false);
+  const styles={
+    color:movie.rating>=8 ? "green":"red"
+  }
   return (
     <div class="movie-container">
       <img className='movie-poster' src={movie.poster} alt="{movie.name}" />
       <div className='movie-name-rating'>
         <h2 className='movie-name'>{movie.name}</h2>
-        <p className='movie-rating'>{movie.rating}</p>
+        <p className='movie-rating' style={styles}>{movie.rating}</p>
       </div>
       <button onClick={() => setShow(!show)}>Toggle the description</button>
       {show ? <p className='movie-summary'>{movie.summary}</p> : null}
