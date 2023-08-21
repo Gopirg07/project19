50bb53241b1fb7700a9ca0a085bdbc8f07232c3e
Gopirg07 <gopinathofficial16@gmail.com>
2023-08-21 22:51:05 +0530

    Server Changed

--- src/global.js
+++ src/global.js
@@ -1,3 +1,3 @@
 // export const API = "https://638f3a564ddca317d7f213a2.mockapi.io";
 // export const API = "http://localhost:4000";
-export const API = "https://module-7-mongo-db.vercel.app";
+export const API = "https://movie-review-v1e8.onrender.com";
